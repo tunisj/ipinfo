6c61d3ce599c5feb187911762f15e9cc3bd6726c
tunisj <77grains@gmail.com>
2019-02-14 14:52:12 -0600

    Update getipinfo.py
    Added argparse functionality to utilize other file names

--- getipinfo.py
+++ getipinfo.py
@@ -1,18 +1,62 @@
 import ipinfo
+import sys
 import csv
+import os
+import argparse
 import config
 
 access_token = config.apikey
 
-outputcsv = open('output.csv', 'wb')    
-outputWriter = csv.writer(outputcsv)
+class getipinfo:
+    def __init__(self, input, output):
+        self.input = input
+        self.output = output
 
-#<!--Write CSV Table Headers--!>
-outputWriter.writerow(['IPADDRESS','LOCATION','CITY','REGION','COUNTRY','ORGANIZATION'])
+    def main(self):
+        outputcsv = open(self.output, 'wb')    
+        outputWriter = csv.writer(outputcsv)
+        outputWriter.writerow(['IPADDRESS','LOCATION','CITY','REGION','COUNTRY','ORGANIZATION'])
+        handler = ipinfo.getHandler(access_token)
+        with open(self.input, 'r') as loa:
+            for ip in loa:
+                details = handler.getDetails(ip)
+                outputWriter.writerow([details.ip,details.loc,details.city,details.region,details.country,details.org])
+            print "Finished"
+        
+        
+if __name__ == "__main__":
+    banner = '''        
+.__       .__        _____       
+|__|_____ |__| _____/ ____\____  
+|  \____ \|  |/    \   __\/  _ \ 
+|  |  |_> >  |   |  \  | (  <_> )
+|__|   __/|__|___|  /__|  \____/ 
+   |__|           \/             
+   by@tunisj
+   '''
+   
+    parser = argparse.ArgumentParser(description='getipinfo by@tunisj')
 
-#<!--Call Api methods--!>
-handler = ipinfo.getHandler(access_token)
-with open('listofipaddresses.txt', 'r') as listofipAddresses:
-    for ipAddress in listofipAddresses:
-        details = handler.getDetails(ipAddress)
-        outputWriter.writerow([details.ip,details.loc,details.city,details.region,details.country,details.org])
+    parser.add_argument(
+        '-i', '--input',
+        action='store',
+        dest='input',
+        help='List of IP Addresses to use',
+        default='listofipaddresses.txt'
+    )
+    parser.add_argument(
+        '-o', '--output',
+        action='store',
+        dest='output',
+        help='Output to save valid results',
+        default='output.csv'
+    )
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+    args = parser.parse_args()
+    
+    print (banner)
+    get_ipinfo = getipinfo(
+        args.input,
+        args.output
+    )
+    get_ipinfo.main()
